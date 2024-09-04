c5393353115f63be36bf66894e2651d589f1bf00
TakuchanTV <fk.53rk2@icloud.com>
2024-09-05 01:39:22 +0900

    テキストを書く欄とテキストを表示させる昨日を実装しました。

--- InputText.jsx
+++ InputText.jsx
@@ -1,3 +1,32 @@
+import { useState } from "react"
+import { NavLink } from "react-router-dom";
 export const InputText = () => {
+  const [text, setText] = useState('');
+　const [addtext, setAddText] = useState('');
+  
+  const handleText = (e) => {
+    setText(e.target.value)
+  }
+
+  const handleAddText = (e) => {
+    setAddText(text)
+    setText('')
+  }
+  return(
+    <div>
+        <h1>InputTextページです</h1>
+
+        <input type="text" value={text} onChange={handleText} />
+
+        <button onClick={handleAddText}>この文章を移動させます。</button>
+
+        <p>{addtext}</p>
+
+        <NavLink to="/">
+         <p>Homeページに移動します</p>  
+       </NavLink>
     
+        
+    </div>
+  )
 }
