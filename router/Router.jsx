a7856100b3c6a9b4f7f3f9eb4263487202b4bf96
TakuchanTV <fk.53rk2@icloud.com>
2024-09-10 23:10:47 +0900

    Routeによってページ遷移先を設定しました

--- router/Router.jsx
+++ router/Router.jsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom"
 import { Home } from "../Home"
 import { CountUp } from "../CountUp"
+import { InputText } from "../InputText";
+import { ShowMunu } from "../ShowMenu";
 
 export const Router = () => {
     return(
     <Routes>
       <Route path="/" element={<Home />} /> 
       <Route  path="CountUp" element={<CountUp />} />
+      <Route path="InputText" element={<InputText />} />
+      <Route path="ShowMenu" element={<ShowMunu />} />
     </Routes>
     )
     
