61a169693ad6a77343b7b8bfdf9d1811146655f4
TakuchanTV <fk.53rk2@icloud.com>
2024-09-15 09:28:14 +0900

    test commit

--- InputText.jsx
+++ InputText.jsx
@@ -1,24 +1,60 @@
-import { useState } from "react"
-import { NavLink } from "react-router-dom";
+import { useEffect, useState } from "react"
+import { NavLink, useNavigate, useParams, useSearchParams } from "react-router-dom";
 export const InputText = () => {
   const [text, setText] = useState('');
 　const [addtext, setAddText] = useState('');
+　const navigate = useNavigate();
+
+const [params, setParams] = useSearchParams();
+
+const Text = params.get('s') || '';
+
+ useEffect(() => {
+  const storedText = localStorage.getItem('addtext');
+  if (storedText) {
+    setAddText(storedText);
+  }
+ },[]);
   
+  const handleparamsText = (e) => {
+    setParams({
+      s: e.target.value
+    })}
+
+
   const handleText = (e) => {
     setText(e.target.value)
   }
-
-  const handleAddText = (e) => {
+  
+  
+  const handleAddText = () => {
     setAddText(text)
     setText('')
+    localStorage.setItem('addtext',addtext)
+  }
+
+  const handleDelete = () => {
+    setAddText('');
+    localStorage.removeItem('addtext',addtext);
+  }
+
+  const NextPage = () => {
+    navigate("/ShowMenu",{
+      state:{
+        Text:Text || '',
+      }
+    })
   }
   return(
     <div>
         <h1>InputTextページです</h1>
 
         <input type="text" value={text} onChange={handleText} />
+        <input type="text" value={Text} onChange={handleparamsText} />
 
         <button onClick={handleAddText}>この文章を移動させます。</button>
+        <button onClick={handleDelete}>この文章を削除します。</button>
+        <button onClick={NextPage}>この文章を遷移先に移動させます。</button>
 
         <p>{addtext}</p>
 
